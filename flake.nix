c872e20405efa4c3df4f72da77833d86b994eeb5
wrvsrx <wrvsrx@outlook.com>
2022-12-16 11:14:42 +0800

    feat(flake): add formatter

--- flake.nix
+++ flake.nix
@@ -1,21 +1,19 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      packages = forAllSystems (system: import ./default.nix {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = import nixpkgs { inherit system; };
-      });
-    };
+      in
+      {
+        packages = import ./default.nix {
+          inherit pkgs;
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
 }
