1b3bdb0f473a3dd4a655feb19603c9d1cacecdf8
wrvsrx <wrvsrx@outlook.com>
2022-12-18 17:18:17 +0800

    fix(pkgs): make some packages build when used as overlay
    These packages are:
    
    - wpa_supplicant
    - pam_ssh_agent_auth

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,6 +10,6 @@ in
     noto-fonts-cjk-serif-fix-weight;
   gen-giraffe-wallpaper = callPackage ./gen-giraffe-wallpaper { };
   xdg-utils-patched = callPackage ./xdg-utils-patched { };
-  pam_ssh_agent_auth = callPackage ./pam_ssh_agent_auth { };
-  wpa_supplicant = callPackage ./wpa_supplicant { };
+  pam_ssh_agent_auth = callPackage ./pam_ssh_agent_auth { inherit (pkgs) pam_ssh_agent_auth; };
+  wpa_supplicant = callPackage ./wpa_supplicant { inherit (pkgs) wpa_supplicant; };
 }
