866dc7ae9ebf23cb13782f9629b57cfd605993cd
wrvsrx <wrvsrx@outlook.com>
2023-01-09 17:57:09 +0800

    feat(packages): use flake for packages providing flake.nix

--- flake.nix
+++ flake.nix
@@ -8,22 +8,44 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
+    giraffe-wallpaper = {
+      url = "github:wrvsrx/giraffe-wallpaper";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    osc52 = {
+      url = "github:wrvsrx/osc52";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, clipcat }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , clipcat
+    , giraffe-wallpaper
+    , osc52
+    }:
     let
-      toPackages = pkgs: import ./pkgs { inherit pkgs; }
-        // { inherit (clipcat.packages.${pkgs.system}) clipcat; };
+      toPackages = pkgs:
+        import ./pkgs { inherit pkgs; }
+        // {
+          clipcat = clipcat.packages.${pkgs.system}.default;
+          giraffe-wallpaper = giraffe-wallpaper.packages.${pkgs.system}.default;
+          osc52 = osc52.packages.${pkgs.system}.default;
+        };
     in
     flake-utils.lib.eachDefaultSystem
       (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        rec {
-          packages = flake-utils.lib.filterPackages pkgs.system (toPackages pkgs);
-          checks = packages;
-          formatter = pkgs.nixpkgs-fmt;
-        }
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        packages = flake-utils.lib.filterPackages pkgs.system (toPackages pkgs);
+        checks = packages;
+        formatter = pkgs.nixpkgs-fmt;
+      }
       ) // {
       overlays.default = final: prev: toPackages prev;
     };
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,10 +8,8 @@ in
   inherit (callPackage ./noto-fonts-cjk { })
     noto-fonts-cjk-sans-fix-weight
     noto-fonts-cjk-serif-fix-weight;
-  giraffe-wallpaper = callPackage ./giraffe-wallpaper { };
   xdg-utils-patched = callPackage ./xdg-utils-patched { };
   pam_ssh_agent_auth = callPackage ./pam_ssh_agent_auth { inherit (pkgs) pam_ssh_agent_auth; };
   wpa_supplicant = callPackage ./wpa_supplicant { inherit (pkgs) wpa_supplicant; };
-  osc52 = callPackage ./osc52 { };
   xclip = callPackage ./xclip { inherit (pkgs) xclip; };
 }
