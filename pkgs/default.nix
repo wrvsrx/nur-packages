5bd9cbc144194897878c1cb8c6b5b573d7d1fa64
wrvsrx <wrvsrx@outlook.com>
2023-03-28 19:44:20 +0800

    feat(pkgs): remove wezterm and wpa_supplicant

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,9 +11,7 @@ in
     noto-fonts-cjk-serif-fix-weight;
   xdg-utils-patched = callPackage ./xdg-utils-patched { };
   pam_ssh_agent_auth = callPackage ./pam_ssh_agent_auth { inherit (pkgs) pam_ssh_agent_auth; };
-  wpa_supplicant = callPackage ./wpa_supplicant { inherit (pkgs) wpa_supplicant; };
   xclip = callPackage ./xclip { inherit (pkgs) xclip; };
-  wezterm = callPackage ./wezterm { inherit (pkgs) wezterm; };
   clipcat = callPackage ./clipcat { source = sources.clipcat; };
   giraffe-wallpaper = callPackage ./giraffe-wallpaper { source = sources.giraffe-wallpaper; };
   osc52 = haskellPackages.callPackage ./osc52 { source = sources.osc52; };
--- pkgs/wpa_supplicant/default.nix
+++
@@ -1,7 +0,0 @@
-{ wpa_supplicant }:
-wpa_supplicant.overrideAttrs (old: {
-  # reference:
-  # https://bugs.launchpad.net/ubuntu/+source/wpa/+bug/1958267
-  # http://launchpadlibrarian.net/605188576/wpa_2%3A2.10-6ubuntu1_2%3A2.10-6ubuntu2.diff.gz
-  patches = old.patches ++ [ ./tls_v1_0.patch ];
-})
