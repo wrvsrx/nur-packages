7faaf5920d14b4e8b5c285fd384d89e2a1c6dcf9
wrvsrx <wrvsrx@outlook.com>
2023-12-25 08:30:42 +0800

    feat(pkgs/pkgs-to-flat-packages): update `meson-patched`

--- pkgs/to-override-packages/meson/default.nix
+++ pkgs/to-override-packages/meson/default.nix
@@ -4,8 +4,8 @@
 meson.overridePythonAttrs (old: {
   patches = old.patches ++ [
     (fetchpatch {
-      url = "https://github.com/mesonbuild/meson/compare/6ccd708f30e3649f6d531eebb8301132cb0f0759...12a186fae1b920aa3b2baddc86c0fe779296917b.patch";
-      hash = "sha256-yYxVdqBacsZ0oJkiRdC4kD1t2ztodQcey2U1iuXmuxI=";
+      url = "https://github.com/mesonbuild/meson/compare/6ccd708f30e3649f6d531eebb8301132cb0f0759...a5678b64be9e9e75ded0a7783f70a9c7e47274d0.patch";
+      hash = "sha256-qc8Nlqzb5jBL5iGBnJ6DG0m9RIUxG11VR2Ss2NXOa9s=";
     })
   ];
 })
