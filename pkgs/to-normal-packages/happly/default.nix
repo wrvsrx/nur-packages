24df6fb375591b79d73acdf700f312b5b80ba33f
wrvsrx <wrvsrx@outlook.com>
2024-01-19 18:57:33 +0800

    feat(pkgs/pkgs-to-flat-packages/happly): add patch to fix build on nvcc

--- pkgs/to-normal-packages/happly/default.nix
+++ pkgs/to-normal-packages/happly/default.nix
@@ -2,10 +2,18 @@
 , meson
 , ninja
 , source
+, fetchpatch
 }:
 stdenv.mkDerivation {
   inherit (source) pname src version;
-  patches = [ ./pc.patch ];
+  patches = [
+    ./pc.patch
+    # fix build on nvcc
+    (fetchpatch {
+      url = "https://github.com/nmwsharp/happly/compare/cfa2611550bc7da65855a78af0574b65deb81766...3bf5fe46279cfabf624037668f30faf4b05720dd.patch";
+      hash = "sha256-9Y5g/XzeInVg4qV1H12QsZ/OSMezXWCz2U6RfH4t/BE=";
+    })
+  ];
   nativeBuildInputs = [
     meson
     ninja
