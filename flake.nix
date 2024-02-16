ded284652165b395841d24bb50f87ec2e1927297
wrvsrx <wrvsrx@outlook.com>
2024-02-16 22:26:07 +0800

    fix(lib): fix variable error in `importFlake`

--- flake.nix
+++ flake.nix
@@ -22,11 +22,11 @@
         lib = {
           # this function is modified from nix official implementation
           # https://github.com/NixOS/nix/blob/56dc6ed8410510033b835d48b3bd22766e8349a0/src/libexpr/flake/call-flake.nix#L7-L61
-          importFlake = flake: inputs:
+          importFlake = path: inputs:
             let
-              flake = import (flake + "/flake.nix");
+              flake = import (path + "/flake.nix");
               outputs = flake.outputs (inputs // { self = result; });
-              result = outputs // { outPath = flake; inherit inputs outputs; _type = "flake"; };
+              result = outputs // { outPath = path; inherit inputs outputs; _type = "flake"; };
             in
             result;
           patchFlake = flake: system: patchesToFetch:
