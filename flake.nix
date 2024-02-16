dda834aa4aac1e61d847a7c66da192a6f13788a4
wrvsrx <wrvsrx@outlook.com>
2024-02-16 22:17:28 +0800

    feat(lib): add `importFlake` and `patchFlake`

--- flake.nix
+++ flake.nix
@@ -19,6 +19,27 @@
       systems = [ "x86_64-linux" ];
       flake = {
         inherit templates;
+        lib = {
+          # this function is modified from nix official implementation
+          # https://github.com/NixOS/nix/blob/56dc6ed8410510033b835d48b3bd22766e8349a0/src/libexpr/flake/call-flake.nix#L7-L61
+          importFlake = flake: inputs:
+            let
+              flake = import (flake + "/flake.nix");
+              outputs = flake.outputs (inputs // { self = result; });
+              result = outputs // { outPath = flake; inherit inputs outputs; _type = "flake"; };
+            in
+            result;
+          patchFlake = flake: system: patchesToFetch:
+            let
+              initial-pkgs = import inputs.nixpkgs { inherit system; };
+              src = initial-pkgs.applyPatches {
+                name = "patched-flake";
+                src = flake;
+                patches = [ (map initial-pkgs.fetchpatch patchesToFetch) ];
+              };
+            in
+            inputs.self.lib.importFlake src flake.inputs;
+        };
         overlays.default = final: prev:
           (pkgs-to-flat-packages prev) // {
             pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [ (pkgs-to-python-modules prev) ];
