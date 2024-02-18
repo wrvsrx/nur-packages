0f6be5cd4f1fed9f0dd2e5dcfc34a8f23377a5d4
wrvsrx <wrvsrx@outlook.com>
2024-02-18 16:23:38 +0800

    feat(pkgs): improve calibre package

--- pkgs/to-override-packages/calibre/fetch-ua.py
+++ pkgs/to-override-packages/calibre/fetch-ua.py
@@ -2,19 +2,13 @@ import bz2
 import ssl
 from urllib.request import urlopen
 import subprocess
-
-
-def download_from_calibre_server(url):
-    ca = 'result/resources/calibre-ebook-root-CA.crt'
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    ssl_context.load_verify_locations(ca)
-    return urlopen(url, context=ssl_context).read()
-
+import json
 
 subprocess.check_call(['nix', 'build', '.#calibre.src'])
-raw = download_from_calibre_server(
-    'https://code.calibre-ebook.com/ua-popularity')
+from result.setup.browser_data import get_data
+data = get_data()
+with open('ua-popularity.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=2, ensure_ascii=False, sort_keys=True)
+    # to pass nixpkgs eol check
+    f.write('\n')
 subprocess.check_call(['rm', 'result'])
-cnt = bz2.decompress(raw).decode('utf-8')
-with open('./ua-popularity.txt', 'w') as f:
-    f.write(cnt)
