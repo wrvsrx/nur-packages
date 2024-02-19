a06eccf2831a3c2cc836a182e257a04dc4499ae1
wrvsrx <wrvsrx@outlook.com>
2024-02-19 18:09:43 +0800

    feat(pkgs): improve coc-zk packaging

--- pkgs/to-vim-plugins/coc-zk/default.nix
+++ pkgs/to-vim-plugins/coc-zk/default.nix
@@ -1,29 +1,18 @@
-{ stdenvNoCC
-, mkYarnPackage
+{ mkYarnPackage
 , source
 }:
-let
-  tgz = mkYarnPackage {
-    inherit (source) pname version src;
-    packageJSON = ./package.json;
-    yarnLock = ./yarn.lock;
-    yarnNix = ./yarn.nix;
-    buildPhase = ''
-      yarn --offline build
-      yarn --offline pack --filename main.tgz
-    '';
-    installPhase = ''
-      mkdir -p $out
-      cp main.tgz $out/$name.tgz
-    '';
-    distPhase = "true";
-  };
-in
-stdenvNoCC.mkDerivation {
-  inherit (source) pname version;
-  src = "${tgz}/${tgz.name}.tgz";
-  installPhase = ''
-    mkdir -p $out
-    mv * $out
+mkYarnPackage {
+  inherit (source) pname version src;
+  packageJSON = ./package.json;
+  yarnLock = ./yarn.lock;
+  yarnNix = ./yarn.nix;
+  buildPhase = ''
+    yarn --offline build
   '';
+  installPhase = ''
+    yarn --offline pack --filename main.tgz
+    mkdir -p $out
+    tar xzf main.tgz --strip-components=1 -C $out
+  '';
+  distPhase = "true";
 }
