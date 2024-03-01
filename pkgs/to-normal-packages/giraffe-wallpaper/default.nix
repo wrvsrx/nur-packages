4a538389c83e17aba9bd077b65180448d89d4796
wrvsrx <wrvsrx@outlook.com>
2024-03-01 22:55:28 +0800

    chore(pkgs/giraffe-wallpaper): use IFD to DRY

--- pkgs/to-normal-packages/giraffe-wallpaper/default.nix
+++ pkgs/to-normal-packages/giraffe-wallpaper/default.nix
@@ -1,23 +1,6 @@
-{ haskellPackages
-, inkscape
-, imagemagick
-, stdenv
-, width ? 3840
+{ width ? 3840
 , height ? 2160
+, callPackage
 , source
 }:
-stdenv.mkDerivation {
-  buildInputs = [
-    haskellPackages.ghc
-    inkscape
-    imagemagick
-  ];
-  inherit (source) pname version src;
-  buildPhase = ''
-    runghc Main main.svg ${builtins.toString width} ${builtins.toString height}
-  '';
-  installPhase = ''
-    mkdir -p $out/share
-    cp output.png $out/share/wallpaper.png
-  '';
-}
+(callPackage source.src { inherit width height; }).overrideAttrs { name = "${source.pname}-${source.version}"; }
