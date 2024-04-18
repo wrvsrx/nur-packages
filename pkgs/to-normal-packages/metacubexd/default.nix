b1ef91277b3bc842cfb36785fb7b4ff6e5a7cfde
wrvsrx <wrvsrx@outlook.com>
2024-04-18 10:15:20 +0800

    fix(pkgs/metacubexd): fix build

--- pkgs/to-normal-packages/metacubexd/default.nix
+++ pkgs/to-normal-packages/metacubexd/default.nix
@@ -1,66 +1,30 @@
 {
-  stdenv,
-  bun,
+  source,
   nodePackages,
   vips,
   lib,
-  source,
   pkg-config,
   python3,
   zlib,
+  mkPnpmPackage,
 }:
 let
-  inherit (source) pname src;
   version = lib.removePrefix "v" source.version;
-  # we build metacubexd in following steps:
-  # 
-  # 1. get cache as a FOD during running `bun install --ignore-scripts` 
-  # 2. use the cache to build `node_modules` (`bun install --force`) and `dist` (`bun run build`)
-  # 
-  # The reason why we use bun cache instead of `node_modules` as FOD is that
-  # 
-  # 1.  If we run `bun install` without `--ignore-scripts`, native libraries in node_modules will contain some store path as reference, which is not allowed in FOD.
-  # 2.  If we run `bun install` with `--ignore-scripts`, we have to build native libraries in step 2. However, bun doesn't provide a method to only run install scripts without redownload like `npm rebuild`.
-  node_modules_cache = stdenv.mkDerivation {
-    pname = pname + "_cache";
-    inherit version src;
-    nativeBuildInputs = [ bun ];
-    impureEnvVars = lib.fetchers.proxyImpureEnvVars;
-    buildPhase = ''
-      mkdir cache
-      export XDG_CACHE_HOME=$PWD/cache
-      # bun's update breaks `bun.lockb` sometimes
-      bun install --verbose --ignore-scripts
-    '';
-    installPhase = "cp -r cache $out";
-    outputHash = "sha256-xuFQajgU+W/k6NIYKuV9AQAGX3OkU0h5SBr+KT1zBi0=";
-    outputHashAlgo = "sha256";
-    outputHashMode = "recursive";
-    dontFixup = true;
-  };
-  out = stdenv.mkDerivation {
-    inherit pname version src;
-    nativeBuildInputs = [
-      bun
-      nodePackages.node-gyp
-      nodePackages.nodejs
-      pkg-config
-      python3
-    ];
-    buildInputs = [
-      vips
-      zlib
-    ];
-    patches = [ ./husky.patch ];
-    buildPhase = ''
-      cp -r ${node_modules_cache} cache
-      export XDG_CACHE_HOME=$PWD/cache
-      chmod +w $PWD/cache/.bun/install/cache
-      # bun's update breaks `bun.lockb` sometimes
-      bun install --verbose
-      bun run build
-    '';
-    installPhase = "cp -r dist $out";
-  };
 in
-out
+mkPnpmPackage {
+  inherit (source) pname src;
+  inherit version;
+  inherit (nodePackages) nodejs;
+  installEnv = {
+    # otherwise, node-gyp will try to download node headers from internet
+    npm_config_nodedir = "${nodePackages.nodejs}";
+  };
+  extraBuildInputs = [
+    nodePackages.node-gyp
+    pkg-config
+    python3
+    vips
+    zlib
+  ];
+  noDevDependencies = true;
+}
