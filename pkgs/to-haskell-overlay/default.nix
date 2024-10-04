3784ab0755371171572c053f26ffde1a18d505f1
wrvsrx <wrvsrx@outlook.com>
2024-10-04 20:06:45 +0800

    fix(pkgs/task-utils): update it

--- pkgs/to-haskell-overlay/default.nix
+++ pkgs/to-haskell-overlay/default.nix
@@ -1,4 +1,7 @@
-{ pkgs, to-sources }:
+{
+  pkgs,
+  to-sources,
+}:
 final: prev:
 let
   sources = to-sources { inherit pkgs; };
@@ -18,10 +21,22 @@ rec {
     inherit callPackage;
     source = sources.osc52;
   };
-  task-utils = callIFD {
-    inherit callPackage;
-    source = sources.task-utils;
-  };
+  task-utils =
+    (callIFD {
+      inherit callPackage;
+      source = sources.task-utils;
+    }).override
+      (old: {
+        doclayout = pkgs.haskell.lib.overrideSrc old.doclayout {
+          src = pkgs.fetchFromGitHub {
+            owner = "jgm";
+            repo = "doclayout";
+            rev = "0.5";
+            hash = "sha256-gTJhoM0WEF+5sbA3bEH+eYAjixNQf1oi2WbcBJpwLZg=";
+          };
+          version = "0.5";
+        };
+      });
   calendar-visualization = callIFD {
     inherit callPackage;
     source = sources.calendar-visualization;
