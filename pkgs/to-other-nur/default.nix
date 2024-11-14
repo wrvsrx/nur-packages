940d0eafc75f4d2bf9089b7a07dc17031b9ec3d2
wrvsrx <wrvsrx@outlook.com>
2024-11-15 03:28:20 +0800

    fix(pkgs/easylpac): fix it

--- pkgs/to-other-nur/default.nix
+++ pkgs/to-other-nur/default.nix
@@ -1,10 +1,20 @@
 { inputs }:
 { pkgs }:
-{
-  inherit (import inputs.linyinfeng { inherit pkgs; })
+let
+  linyinfeng = import inputs.linyinfeng { inherit pkgs; };
+  ilya-fedin = import inputs.ilya-fedin { inherit pkgs; };
+in
+rec {
+  inherit (linyinfeng)
     wemeet
+    lpac
     ;
-  inherit (import inputs.ilya-fedin { inherit pkgs; })
+  easylpac = linyinfeng.easylpac.overrideAttrs {
+    postInstall = ''
+      cp ${lpac}/bin/lpac $out/bin/
+    '';
+  };
+  inherit (ilya-fedin)
     qt5ct
     qt6ct
     ;
