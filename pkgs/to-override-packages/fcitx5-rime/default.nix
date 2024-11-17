fa42bfd3a2eb177fe3bbd35a8f0f840073a024ba
wrvsrx <wrvsrx@outlook.com>
2024-11-17 10:07:51 +0800

    fix(pkgs/{fcitx5-rime,librime}): fix build

--- pkgs/to-override-packages/fcitx5-rime/default.nix
+++ pkgs/to-override-packages/fcitx5-rime/default.nix
@@ -4,35 +4,15 @@
   librime,
 }:
 let
-  fcitx5-rime' = fcitx5-rime.override (old: {
-    stdenv = old.stdenv // {
-      mkDerivation =
-        x:
-        old.stdenv.mkDerivation (
-          x
-          // {
-            patches = [
-              (fetchpatch {
-                # https://github.com/wrvsrx/fcitx5-rime/tree/tag_support-set-data-dir-via-xdg-2
-                url = "https://github.com/wrvsrx/fcitx5-rime/compare/fd8bf83dcd731eacd095f8b01cd2ea1f9e9aa429...80955b33e33cb85de9e33052d8e42f8d6822613b.patch";
-                hash = "sha256-Pgz/+3XkAxbwpIS3cnmfaAz9eHcrNFqSq8n8EkSiz1Q=";
-              })
-              ./restore-ascii.patch
-            ];
-            postInstall = ''
-              cp -r "$rimeDataDrv/share/rime-data/." $out/share/rime-data/
-            '';
-          }
-        );
-    };
-    symlinkJoin =
-      x:
-      old.symlinkJoin (
-        x
-        // {
-          postBuild = "mkdir -p $out/share/rime-data";
-        }
-      );
+  fcitx5-rime' = fcitx5-rime.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      (fetchpatch {
+        # https://github.com/wrvsrx/fcitx5-rime/tree/tag_support-set-data-dir-via-xdg-2
+        url = "https://github.com/wrvsrx/fcitx5-rime/compare/fd8bf83dcd731eacd095f8b01cd2ea1f9e9aa429...80955b33e33cb85de9e33052d8e42f8d6822613b.patch";
+        hash = "sha256-Pgz/+3XkAxbwpIS3cnmfaAz9eHcrNFqSq8n8EkSiz1Q=";
+      })
+      ./restore-ascii.patch
+    ];
   });
 in
 fcitx5-rime'.override {
--- pkgs/to-override-packages/librime/default.nix
+++ pkgs/to-override-packages/librime/default.nix
@@ -1,1 +1,9 @@
-{ librime }: librime.overrideAttrs { patches = [ ./save_options.patch ]; }
+{ librime, fetchpatch }:
+librime.overrideAttrs {
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/wrvsrx/librime/compare/c7ab6390c143a11d670f3add41218111edb883c9...fe9eb9a94d753e9e6837b0e982ce9cc80028dfc9.patch";
+      hash = "sha256-1dnHYUZf626C/ByXVtsgWpFql2VEwQQXXS+I3FkLqHA=";
+    })
+  ];
+}
