398519778d87f39212a40517978cd264132b12ac
wrvsrx <wrvsrx@outlook.com>
2024-11-21 03:20:17 +0800

    fix(pkgs/to-python-overlay): fix overlay

--- pkgs/to-python-overlay/default.nix
+++ pkgs/to-python-overlay/default.nix
@@ -3,22 +3,23 @@ let
   sources = to-sources { inherit pkgs; };
 in
 final: prev: rec {
-  autobean-format = prev.callPackage ./autobean-format {
+  # we can only use `final.callPackage` here if we want the overlay works well
+  autobean-format = final.callPackage ./autobean-format {
     source = sources.autobean-format;
     inherit autobean-refactor;
   };
-  autobean-refactor = prev.callPackage ./autobean-refactor { source = sources.autobean-refactor; };
-  compdb = prev.callPackage ./compdb { source = sources.compdb; };
-  OpenEXR = prev.callPackage ./OpenEXR { source = sources.OpenEXR; };
-  jraph = prev.callPackage ./jraph { source = sources.jraph; };
-  jax-md = prev.callPackage ./jax-md {
+  autobean-refactor = final.callPackage ./autobean-refactor { source = sources.autobean-refactor; };
+  compdb = final.callPackage ./compdb { source = sources.compdb; };
+  OpenEXR = final.callPackage ./OpenEXR { source = sources.OpenEXR; };
+  jraph = final.callPackage ./jraph { source = sources.jraph; };
+  jax-md = final.callPackage ./jax-md {
     source = sources.jax-md;
     inherit e3nn-jax jraph;
   };
-  e3nn-jax = prev.callPackage ./e3nn-jax {
+  e3nn-jax = final.callPackage ./e3nn-jax {
     source = sources.e3nn-jax;
   };
-  googletrans = prev.callPackage ./googletrans {
+  googletrans = final.callPackage ./googletrans {
     inherit (prev) googletrans;
     source = sources.googletrans;
   };
