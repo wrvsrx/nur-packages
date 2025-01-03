24706263837db54cb0982c40913bd8a6a057b0a0
wrvsrx <wrvsrx@outlook.com>
2025-01-03 23:59:45 +0800

    feat(pkgs/rimePackages/rime-fcitx5): add version

--- pkgs/rime-packages/rime-fcitx5/default.nix
+++ pkgs/rime-packages/rime-fcitx5/default.nix
@@ -1,5 +1,12 @@
-{ fcitx5-rime, runCommand }:
-runCommand "rime-fcitx5" { } ''
-  mkdir -p $out/share/rime-data
-  cp -r ${fcitx5-rime}/share/rime-data/fcitx5.yaml $out/share/rime-data/
-''
+{
+  stdenvNoCC,
+  fcitx5-rime,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "rime-fcitx5";
+  inherit (fcitx5-rime) src version;
+  installPhase = ''
+    mkdir -p $out/share/rime-data
+    cp -r data/fcitx5.yaml $out/share/rime-data/
+  '';
+}
