4e0be830f9549dd8e0b048c57bb86531a6122b2a
wrvsrx <wrvsrx@outlook.com>
2025-01-05 00:56:52 +0800

    feat(pkgs/SillyTavern): reduce closure size

--- pkgs/SillyTavern/default.nix
+++ pkgs/SillyTavern/default.nix
@@ -13,6 +13,7 @@ buildNpmPackage {
   installPhase = ''
     mkdir -p $out/{bin,lib}
     mv * $out/lib
+    rm $out/lib/node_modules/.package-lock.json
     cat > $out/bin/SillyTavern <<- EOF
     #!${bash}/bin/bash
 
