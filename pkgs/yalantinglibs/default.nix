c31559fde945a604b3c19e9fd3c3476c05a78436
wrvsrx <wrvsrx@outlook.com>
2025-01-20 17:37:44 +0800

    feat(pkgs/yalantinglibs): remove v prefix in version

--- pkgs/yalantinglibs/default.nix
+++ pkgs/yalantinglibs/default.nix
@@ -1,9 +1,11 @@
 {
   stdenv,
   cmake,
+  lib,
   source,
 }:
 stdenv.mkDerivation {
-  inherit (source) pname src version;
+  inherit (source) pname src;
+  version = lib.removePrefix "v" source.version;
   nativeBuildInputs = [ cmake ];
 }
