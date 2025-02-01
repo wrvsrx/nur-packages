9f1edbd4a41f48c13da57a46c2f4464eed9aecc5
wrvsrx <wrvsrx@outlook.com>
2025-02-01 11:11:42 +0800

    feat(pkgs/meson'): update patch

--- pkgs/meson-patched/default.nix
+++ pkgs/meson-patched/default.nix
@@ -3,8 +3,8 @@ meson.overrideAttrs (old: {
   patches = (old.patches or [ ]) ++ [
     # https://github.com/mesonbuild/meson/pull/14178
     (fetchpatch {
-      url = "https://github.com/wrvsrx/meson/compare/tag_fix-mformat-6%5E...tag_fix-mformat-6.patch";
-      hash = "sha256-BQKx36Kz6HhMDoOxazDRz7KAnRhanMhehGlMwnW7PdU=";
+      url = "https://github.com/wrvsrx/meson/compare/tag_fix-mformat-9%5E...tag_fix-mformat-9.patch";
+      hash = "sha256-AR+c9zakQO5oEqmOeoi859K+9qXyRwgMjJr+Uq1l2IA=";
     })
   ];
 })
