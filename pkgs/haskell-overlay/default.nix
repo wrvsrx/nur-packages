4648c3047220b2acc28ef19c93d4ec77dbd739c7
wrvsrx <wrvsrx@outlook.com>
2025-02-17 21:05:55 +0800

    feat(pkgs/haskellPackages): remove IFD

--- pkgs/haskell-overlay/default.nix
+++ pkgs/haskell-overlay/default.nix
@@ -2,22 +2,8 @@
 final: prev:
 let
   inherit (prev) callPackage;
-  callIFD = import ../callIFD.nix;
 in
 {
-  osc52 = callIFD rec {
-    inherit callPackage;
-    source = sources.osc52;
-    version = "0-unstable-" + source.date;
-  };
-  task-utils = callIFD rec {
-    inherit callPackage;
-    version = "0-unstable-" + source.date;
-    source = sources.task-utils;
-    transform =
-      x:
-      x.override (old: {
-        doclayout = prev.doclayout_0_5;
-      });
-  };
+  osc52 = callPackage ./osc52 { source = sources.osc52; };
+  task-utils = callPackage ./task-utils { source = sources.task-utils; };
 }
