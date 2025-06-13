b8a7b61651efc09069dff488617b2d9096a80432
wrvsrx <wrvsrx@outlook.com>
2025-06-14 01:36:38 +0800

    fix(pkgs/niri): fix version check

--- pkgs/niri/default.nix
+++ pkgs/niri/default.nix
@@ -1,4 +1,8 @@
-{ niri, fetchFromGitHub }:
+{
+  niri,
+  fetchFromGitHub,
+  replaceVars,
+}:
 niri.overrideAttrs (
   finalAttrs: oldAttrs: {
     version = "25.05.1-wrvsrx-patched.01";
@@ -8,5 +12,8 @@ niri.overrideAttrs (
       tag = "v${finalAttrs.version}";
       sha256 = "sha256-x32qRthAvr2LewwTjfb5D8/2XBiHVRz3HHymCLLhRsM=";
     };
+    patches = [
+      (replaceVars ./niri-version.patch { inherit (finalAttrs) version; })
+    ];
   }
 )
