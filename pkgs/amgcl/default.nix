edd597797703891d60ecd1cdd31c5d9209e40a9f
wrvsrx <wrvsrx@outlook.com>
2025-10-07 21:17:52 +0800

    feat(pkgs/amgcl,cnpy,hougeo): add CMAKE_POLICY_VERSION_MINIMUM cmake flag

--- pkgs/amgcl/default.nix
+++ pkgs/amgcl/default.nix
@@ -1,9 +1,11 @@
 {
   stdenv,
   cmake,
+  lib,
   source,
 }:
 stdenv.mkDerivation {
   inherit (source) pname src version;
   nativeBuildInputs = [ cmake ];
+  cmakeFlags = [ (lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.5") ];
 }
