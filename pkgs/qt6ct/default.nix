001083e0fa457e14bf038af69bd9cf66fbce95aa
wrvsrx <wrvsrx@outlook.com>
2025-10-23 12:11:51 +0800

    fix(pkgs/qt6ct): fix build

--- pkgs/qt6ct/default.nix
+++ pkgs/qt6ct/default.nix
@@ -1,37 +1,20 @@
 {
   qt6ct,
-  cmake,
-  fetchFromGitHub,
   qtdeclarative,
   kconfig,
   kcolorscheme,
   kiconthemes,
-  wrapQtAppsHook,
-  qttools,
-  qtbase,
 }:
-qt6ct.overrideAttrs (oldAttrs: {
-  src = fetchFromGitHub {
-    owner = "ilya-fedin";
-    repo = "qt6ct";
-    tag = oldAttrs.version;
-    sha256 = "sha256-ePY+BEpEcAq11+pUMjQ4XG358x3bXFQWwI1UAi+KmLo=";
-  };
 
-  buildInputs = oldAttrs.buildInputs ++ [
+qt6ct.overrideAttrs (oldAttrs: {
+  buildInputs = oldAttrs.buildInputs ++ ([
     qtdeclarative
     kconfig
     kcolorscheme
     kiconthemes
-  ];
+  ]);
 
-  nativeBuildInputs = [
-    cmake
-    wrapQtAppsHook
-    qttools
-  ];
-
-  cmakeFlags = [
-    "-DPLUGINDIR=${placeholder "out"}/${qtbase.qtPluginPrefix}"
+  patches = [
+    ./qt6ct-shenanigans.patch
   ];
 })
