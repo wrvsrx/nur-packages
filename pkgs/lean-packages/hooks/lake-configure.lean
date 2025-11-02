ac18676f9970e336965b943d7f0f63472ae24c45
wrvsrx <wrvsrx@outlook.com>
2025-11-02 12:08:10 +0800

    feat(pkgs/lean-packages): refactor lake-configure hook

--- pkgs/lean-packages/hooks/lake-configure.lean
+++ pkgs/lean-packages/hooks/lake-configure.lean
@@ -7,37 +7,30 @@ structure ManifestOverride where
   dir: System.FilePath
   deriving Lean.ToJson, Lean.FromJson, Inhabited
 
-example (l : Lean.Name) (r : Lean.Name) : Bool := l == r
-
 def overrideManifest (manifestOverrides : Array ManifestOverride) (manifest : Lake.Manifest) : Lake.Manifest :=
   let packagesOverrided :=
     manifest.packages.map (
       fun packageEntry =>
         let optionOverride := manifestOverrides.find? (fun manifestOverride => manifestOverride.name == packageEntry.name) 
-        match optionOverride with
-          | some x => {packageEntry with src := Lake.PackageEntrySrc.path x.dir} 
-          | none => packageEntry
+        optionOverride.elim
+          packageEntry
+          fun override => {packageEntry with src := Lake.PackageEntrySrc.path override.dir}
     )
   {manifest with packages := packagesOverrided}
 
 def patchManifest : IO Unit := do
   let manifestString ← IO.FS.readFile "lake-manifest.json"
-  let manifestParseResult : Except String Lake.Manifest := do
+  let manifest : Lake.Manifest ← IO.ofExcept do
     let x ← Lean.Json.parse manifestString
     Lean.FromJson.fromJson? x
-  let manifest ← match manifestParseResult with
-    | Except.ok x => pure x
-    | Except.error x => throw (IO.Error.userError x)
 
-  let manifestOverridePath  ← IO.getEnv "NIX_LAKE_MANIFEST_OVERRIDE" >>= (pure ∘ System.FilePath.mk ∘  Option.get!)
+  let manifestOverridePath ← do
+    let x ← IO.getEnv "NIX_LAKE_MANIFEST_OVERRIDE"
+    x.elim (throw (IO.Error.userError "no such environment variable")) pure
   let manifestOverrideString ← IO.FS.readFile manifestOverridePath
-  let manifestOverrideParseResult : Except String (Array ManifestOverride) := do
+  let manifestOverride : (Array ManifestOverride) ← IO.ofExcept do
     let x ← Lean.Json.parse manifestOverrideString
     Lean.FromJson.fromJson? x
-  let manifestOverride ← match manifestOverrideParseResult with
-    | Except.ok x => pure x
-    | Except.error x => throw (IO.Error.userError x)
-
   let manifestOverrided := overrideManifest manifestOverride manifest
 
   let manifestOverridedStr := toString (Lean.ToJson.toJson manifestOverrided) 
