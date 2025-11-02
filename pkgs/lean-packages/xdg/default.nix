c1b1c70cf961eb43bd014b38e6609410b00283be
wrvsrx <wrvsrx@outlook.com>
2025-11-02 12:50:54 +0800

    feat(pkgs/lean-packages/xdg): 0.1.0 -> 0.2.0

--- pkgs/lean-packages/xdg/default.nix
+++ pkgs/lean-packages/xdg/default.nix
@@ -7,12 +7,12 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "xdg";
-  version = "0.1.0";
+  version = "0.2.0";
   src = fetchFromGitHub {
     owner = "wrvsrx";
     repo = "xdg";
     tag = finalAttrs.version;
-    hash = "sha256-oCyfOGKk+tjMm1xRmTz90gKW3bXQdBqXvVHbqCuhlO0=";
+    hash = "sha256-HKD3zchrhuqHM6fjmFQMardb9ov3GG7rki2kjW8oLas=";
   };
   env.NIX_LAKE_MANIFEST_OVERRIDE = writeText "lake-manifest-overrided.json" (builtins.toJSON [ ]);
   nativeBuildInputs = [
