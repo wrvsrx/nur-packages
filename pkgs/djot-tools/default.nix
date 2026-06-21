5fe5de5d03f3bc2e8cfde9b95775709bcd09afce
wrvsrx <wrvsrx@outlook.com>
2026-06-21 15:32:01 +0800

    feat(pkgs/djot-tools): 0.7.0 -> 0.8.0

--- pkgs/djot-tools/default.nix
+++ pkgs/djot-tools/default.nix
@@ -4,14 +4,14 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "djot-tools";
-  version = "0.7.0";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "wrvsrx";
     repo = "djot-language-server";
     tag = finalAttrs.version;
-    hash = "sha256-GWmhoFHjMNxuNfgY1zWeWPU7zauO82pBWukggezy7co=";
+    hash = "sha256-dU+RgdO5x8OwGcJKStdE+vOOW6PILGH6yNzRwgnbvwQ=";
   };
 
-  cargoHash = "sha256-EwLjms9NXm1B1xmwYUBgk8SaqabSjCoBs1kLfWyxBSg=";
+  cargoHash = "sha256-8tVSurLdWZN4EJ0kOuZOT/PkFX6nJWs4dYFUwlZ5xb8=";
 })
