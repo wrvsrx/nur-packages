f3144cd87ed1a7f2f10058733585a3de57650bac
wrvsrx <wrvsrx@outlook.com>
2026-09-11 14:11:36 +0800

    fix(pkgs/fcitx5): use 5.1.21 patch tag

--- pkgs/fcitx5/default.nix
+++ pkgs/fcitx5/default.nix
@@ -5,9 +5,8 @@
 fcitx5.overrideAttrs (old: {
   patches = (old.patches or [ ]) ++ [
     (fetchpatch {
-      # https://github.com/wrvsrx/fcitx5/compare/5.1.21..5.1.21+wayland-flush-v2.diff
-      url = "https://github.com/wrvsrx/fcitx5/compare/5.1.21..5.1.21+wayland-flush-v3.diff";
-      hash = "sha256-WHmtzopPzilHwyLzBKRMeF2UDWmbdanF9bVxZtk93G8=";
+      url = "https://github.com/wrvsrx/fcitx5/compare/wayland-flush-5.1.21-v2^..wayland-flush-5.1.21-v2.diff";
+      hash = "sha256-IOZFEhHVGTDe7Ut7NlrZEwbxzY7a5IBlgMsTu/8JUT0=";
     })
   ];
 })
