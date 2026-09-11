ef43f5fe27d2175f823e331fd8135a4b8c88383b
wrvsrx <wrvsrx@outlook.com>
2026-09-11 13:45:17 +0800

    fix(pkgs/fcitx5): flush before wayland disconnect

--- pkgs/fcitx5/default.nix
+++ pkgs/fcitx5/default.nix
@@ -5,9 +5,9 @@
 fcitx5.overrideAttrs (old: {
   patches = (old.patches or [ ]) ++ [
     (fetchpatch {
-      # https://github.com/fcitx/fcitx5/pull/1667
-      url = "https://github.com/wrvsrx/fcitx5/compare/5.1.21..5.1.21+waylandim-teardown-v1.diff";
-      hash = "sha256-wJrQWdjO1g22CJExR4ti4pgpGPavIsVq9xQyHge+vGY=";
+      # https://github.com/wrvsrx/fcitx5/compare/5.1.21..5.1.21+wayland-flush-v2.diff
+      url = "https://github.com/wrvsrx/fcitx5/compare/5.1.21..5.1.21+wayland-flush-v2.diff";
+      hash = "sha256-kWmPi+0bolR8MVu9aFSInkZtBpKQB/46nbzH/dhtGyw=";
     })
   ];
 })
